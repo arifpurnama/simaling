06d0d7e3ba2f52dec73a9b25627ccd4afd417ce5
Saefulbahrisaja <saefulbahrisaja@gmail.com>
2019-10-13 18:47:17 -0700

    Update profil OK

--- app/src/main/java/com/bahri/simaling/LaporanFragment.java
+++ app/src/main/java/com/bahri/simaling/LaporanFragment.java
@@ -5,17 +5,23 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bahri.simaling.Utils.Userparselable;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LaporanFragment extends Fragment {
-
-
+    private TextView nama,ktp;
+    private int ident;
+    private Userparselable user;
     public LaporanFragment() {
         // Required empty public constructor
     }
@@ -25,7 +31,25 @@ public class LaporanFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_laporan, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        nama=(TextView) view.findViewById(R.id.nama_profile);
+        ktp=(TextView) view.findViewById(R.id.nik_profile);
+
+        try{
+            Bundle bundle = getActivity().getIntent().getExtras();
+            user = bundle.getParcelable("datauser");
+            if(bundle!=null){
+                ident = user.getId();
+                nama.setText(user.getNama());
+                ktp.setText(user.getNik());
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+        return view;
     }
 
 }
