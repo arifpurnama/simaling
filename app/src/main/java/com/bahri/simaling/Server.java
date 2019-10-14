37541c4e169151d31f58e831e8263952c2bda999
Saefulbahrisaja <saefulbahrisaja@gmail.com>
2019-10-13 22:11:51 -0700

    Update profil OK

---
+++ app/src/main/java/com/bahri/simaling/Server.java
@@ -0,0 +1,6 @@
+package com.bahri.simaling;
+
+public class Server {
+    // sesuaikan dengan IP Address PC/laptop atau ip address emulator android 10.0.2.2
+    public static final String URL = "http://192.168.10.177/Lingkungan/Api/";
+}
