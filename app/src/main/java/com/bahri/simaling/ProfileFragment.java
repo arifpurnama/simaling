93e92fd6bc888b235fdc069430a37b2157858abb
Saefulbahrisaja <saefulbahrisaja@gmail.com>
2019-10-13 18:33:20 -0700

    Update

--- app/src/main/java/com/bahri/simaling/ProfileFragment.java
+++ app/src/main/java/com/bahri/simaling/ProfileFragment.java
@@ -1,18 +1,29 @@
 package com.bahri.simaling;
 
 
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import com.bumptech.glide.Glide;
+import com.bahri.simaling.Utils.Userparselable;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
+private TextView nama,ktp,alamat,rt,rw,kel,kab,negara,gambar;
+    private int ident;
+    private Userparselable user;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -22,7 +33,41 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        nama=(TextView) view.findViewById(R.id.nama_profile);
+        ktp=(TextView) view.findViewById(R.id.nik_profile);
+        alamat=(TextView) view.findViewById(R.id.alamat_profile);
+        rt=(TextView) view.findViewById(R.id.rt_profile);
+        rw=(TextView) view.findViewById(R.id.rw_profile);
+        kel=(TextView) view.findViewById(R.id.kelurahan_profile);
+        kab=(TextView) view.findViewById(R.id.kota_profile);
+        negara=(TextView) view.findViewById(R.id.kewarganegaraan_profile);
+
+
+        try{
+            Bundle bundle = getActivity().getIntent().getExtras();
+            user = bundle.getParcelable("datauser");
+            if(bundle!=null){
+                ident = user.getId();
+                nama.setText(user.getNama());
+                ktp.setText(user.getNik());
+                //if(!user.getImage().equals("Gambar Kosong")){
+                 //   String url_image = "http://192.168.1.5/lingkunga/"+user.getImage();
+                 //   url_image = url_image.replace(" ","%20");
+                 //   try {
+                 //       Log.i("RESPUESTA IMAGE: ",""+url_image);
+                 //       Glide.with(this).load(url_image).into(photo);
+                 //   } catch (Exception e) {
+                  //      e.printStackTrace();
+                 //   }
+              //  }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return view;
     }
 
 }
